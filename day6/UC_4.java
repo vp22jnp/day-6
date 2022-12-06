d861cdb87f24979de87f18ff06ee0f7b0ba6096b
vp22j <vp22jnp@gmail.com>
2022-12-06 23:53:06 +0530

    added: to find a reverse number

---
+++ day6/UC_4.java
@@ -0,0 +1,16 @@
+package day6;
+
+public class UC_4 {
+    public static void main(String[] args)
+    {
+        int number = 987654, reverse = 0;
+        while(number != 0)
+        {
+            int remainder = number % 10;
+            reverse = reverse * 10 + remainder;
+            number = number/10;
+        }
+        System.out.println("The reverse of the given number is: " + reverse);
+    }
+}
+
